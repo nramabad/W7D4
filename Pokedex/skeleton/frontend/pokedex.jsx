112772bf1248a107f03ccee9fd250f2629da8daf
nramabad <nramabad@gmail.com>
2018-12-05 16:32:19 -0800

    Finish phase 0 and 1 of Pokedex

---
+++ Pokedex/skeleton/frontend/pokedex.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(
+    <h1>Pokedex</h1>,
+    rootEl
+  );
+});
